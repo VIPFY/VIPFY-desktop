7eec1cd409b1f6585e867c189f9068b213dd8201
Jannis Froese <jf@vipfy.com>
2018-03-01 18:40:07 +0100

    more flexible preload script import

--- src/preload.js
+++ src/preload.js
@@ -1,9 +1,30 @@
 const con = require('electron').remote.getGlobal('console');
 
-try {
-  require(`./locationScripts/${window.location.hostname}.ts`)();
-  con.log(`Executed Script for ${window.location.hostname}`);
-} catch(e) {
-  con.log(e.stack);
-  con.log(`No Script for ${window.location.hostname}`);
+const scripts = {
+  "(www.)?vipfy.com": "vipfy.ts",
+  "(www.)?dropbox.com": "dropbox.ts",
+}
+
+const hostname = window.location.hostname;
+
+let script = ""
+for (const domain in scripts) {
+  if(new RegExp(domain).test(hostname)) {
+    script = scripts[domain]
+    break
+  }
+}
+
+con.log(script);
+
+if(script == "") {
+  con.log(`No Script for ${hostname}`);
+} else {
+  try {
+    require(`./locationScripts/${script}`)();
+    con.log(`Executed Script for ${hostname}`);
+  } catch(e) {
+    con.log(e.stack);
+    con.log(`Executed Script for ${hostname}`);
+  }
 }
