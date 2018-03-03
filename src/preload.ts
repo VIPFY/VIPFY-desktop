7d5c4ed46d931e99d3ee536e9d6927bbfd408a43
Jannis Froese <jf@vipfy.com>
2018-03-03 17:06:57 +0100

    new linter rules

--- src/preload.ts
+++ src/preload.ts
@@ -1,18 +1,19 @@
-const con = require('electron').remote.getGlobal('console');
+// tslint:disable:no-var-requires
+const con = require("electron").remote.getGlobal("console");
 const hostname = window.location.hostname;
 function hostMatches(domain: string) {
   return new RegExp(domain).test(hostname);
 }
 
 try {
-  if (hostMatches('(www.)?vipfy.com')) {
-    require('./locationScripts/vipfy.js')();
-  } else if (hostMatches('(www.)?dropbox.com')) {
-    require('./locationScripts/dropbox.js')();
-  } else if (hostMatches('.*\.?pipedrive.com')) {
-    require('./locationScripts/pipedrive.js')();
-  } else if (hostMatches('.*\.?wrike.com')) {
-    require('./locationScripts/wrike.js')();
+  if (hostMatches("(www.)?vipfy.com")) {
+    require("./locationScripts/vipfy.js")();
+  } else if (hostMatches("(www.)?dropbox.com")) {
+    require("./locationScripts/dropbox.js")();
+  } else if (hostMatches(".*\.?pipedrive.com")) {
+    require("./locationScripts/pipedrive.js")();
+  } else if (hostMatches(".*\.?wrike.com")) {
+    require("./locationScripts/wrike.js")();
   } else {
     con.log(`No Script for ${hostname}`);
   }
