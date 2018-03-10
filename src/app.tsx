4809cf413ee2a91bea2f5431ce38f7684720ad26
Jannis Froese <jf@vipfy.com>
2018-03-10 23:18:31 +0100

    Remove example text

--- src/app.tsx
+++ src/app.tsx
@@ -4,7 +4,6 @@ export class App extends React.Component<undefined, undefined> {
   render() {
     return (
       <div>
-        <h2>Welcome to React with Typescript!</h2>
       </div>
     );
   }
