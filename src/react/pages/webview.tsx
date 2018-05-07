cc3497762b7f7f82c4305ded492edc95e6c1c589
Jannis Froese <jf@vipfy.com>
2018-05-07 19:54:23 +0200

    Actually show loading screen

--- src/react/pages/webview.tsx
+++ src/react/pages/webview.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import {Component, ReactPropTypes} from "react";
 import { Link } from "react-router-dom";
 import WebView = require('react-electron-web-view');
+const {shell} = require('electron');
 
 export type WebViewState = {
   url: string;
@@ -47,13 +48,33 @@ export class Webview extends Component<WebViewProps, WebViewState> {
     console.log("URL", url)
   }
 
+  hideLoadingScreen(): void {
+    this.setState({ showLoadingScreen: false });
+  }
+
+  showLoadingScreen(): void {
+    this.setState({ showLoadingScreen: true });
+  }
+
+  onNewWindow(e): void {
+    //if webview tries to open new window, open it in default browser
+    //TODO: probably needs more fine grained control for cases where new window should stay logged in
+    const protocol = require('url').parse(e.url).protocol
+    if (protocol === 'http:' || protocol === 'https:') {
+      shell.openExternal(e.url)
+    }
+  }
+
   render() {
     console.log("WEBVIEW", this.props.app)
     return (
       <div>WEBVIEW
         <WebView id="webview" preload="./preload-launcher.js" webpreferences="webSecurity=no" className="mainPosition"
           src={this.state.url} partition="persist:services" onDidNavigate={e => this.onDidNavigate(e.target.src)}
-          style={{display: this.state.showLoadingScreen ? 'none' : 'block' }}></WebView>
+          style={{display: this.state.showLoadingScreen ? 'none' : 'block' }}
+          onDidFailLoad={(code, desc, url, isMain) => {this.hideLoadingScreen(); console.log(`failed loading ${url}: ${code} ${desc}`)}}
+          onDidStartLoading={e => this.showLoadingScreen()} onDidStopLoading={e => this.hideLoadingScreen()}
+          onNewWindow={e => this.onNewWindow(e)}></WebView>
         <div id="loadingScreen" className="mainPosition" style={{display: this.state.showLoadingScreen ? 'block' : 'none' }}>
           <div className="loadingTextBlock">
             <div className="centerText inspirationalText"><div>{this.state.inspirationalText}</div></div>
