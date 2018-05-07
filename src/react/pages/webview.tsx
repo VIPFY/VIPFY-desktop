ae809cf86f00e0ae0b9a65c14374d10fb1a01699
Jannis Froese <jf@vipfy.com>
2018-05-07 15:12:38 +0200

    Fix merge conflict

--- src/react/pages/webview.tsx
+++ src/react/pages/webview.tsx
@@ -2,13 +2,13 @@ import * as React from "react";
 import {Component} from "react";
 import { Link } from "react-router-dom";
 
-class Webview extends Component {
+class Webview extends Component<any, any> {
 
   render() {
 
     return (
       <div>WEBVIEW
-        <Link to="/area/bug">LINK</Link>
+         <webview id="webview" className="mainPosition" preload="./preload-launcher.js" webpreferences="webSecurity=no" src="https://vipfy.com" partition="persist:services"></webview>
       </div>
     );
   }
