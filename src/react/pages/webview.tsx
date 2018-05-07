8bbfb5b693871bd78e22b9fd2a842e5acfb4694b
NilsVo <nils.vossebein@gmx.de>
2018-05-07 15:56:05 +0200

    small Change

--- src/react/pages/webview.tsx
+++ src/react/pages/webview.tsx
@@ -7,8 +7,9 @@ class Webview extends Component<any, any> {
   render() {
     console.log("WEBVIEW", this.props.app)
     return (
-      <div>WEBVIEW
-         <webview id="webview" className="mainPosition" preload="./preload-launcher.js" webpreferences="webSecurity=no" src="https://vipfy.com" partition="persist:services"></webview>
+      <div>
+         <webview id="webview" className="mainPosition" preload="./preload-launcher.js"
+            webpreferences="webSecurity=no" src="https://vipfy.com" partition="persist:services"></webview>
       </div>
     );
   }
