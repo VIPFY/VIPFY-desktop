5f10a53923ebfd23411f401609159ba766d90a34
Jannis Froese <jf@vipfy.com>
2018-05-23 12:18:12 +0200

    formatting changes

--- src/react/app.tsx
+++ src/react/app.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { Component } from "react";
 import { Route, Switch } from "react-router-dom";
-import { withRouter, Redirect} from "react-router";
+import { withRouter, Redirect } from "react-router";
 import { graphql, compose } from "react-apollo";
 import gql from "graphql-tag";
 
@@ -28,85 +28,101 @@ class App extends Component {
     firstname: "",
     lastname: "",
     admin: false
-  }
+  };
 
   logMeOut = () => {
-    this.setState({login:  false})
+    this.setState({ login: false });
     localStorage.setItem("token", "");
     localStorage.setItem("refreshToken", "");
-    this.props.history.push("/")
-  }
+    this.props.history.push("/");
+  };
 
   setName = (firstname, lastname) => {
-    console.log("SETNAME", firstname, lastname)
-    this.setState({firstname: firstname})
-    this.setState({lastname: lastname})
-  }
+    console.log("SETNAME", firstname, lastname);
+    this.setState({ firstname: firstname });
+    this.setState({ lastname: lastname });
+  };
 
   logMeIn = async (email, password) => {
     try {
-      const res = await this.props.signIn({variables: { email, password }})
-        const { ok, token, refreshToken, user} = res.data.signIn;
-        if (ok) {
-          console.log("SIGNIN", user)
-          localStorage.setItem("token", token);
-          localStorage.setItem("refreshToken", refreshToken);
-          this.setState({login:  true})
-          this.setState({firstname: user.firstname})
-          this.setState({lastname: user.lastname})
-          this.setState({admin: user.admin})
-          this.setState({profilepicture: user.company.profilepicture})
-          this.setState({employees: user.company.employees})
-        }
+      const res = await this.props.signIn({ variables: { email, password } });
+      const { ok, token, refreshToken, user } = res.data.signIn;
+      if (ok) {
+        console.log("SIGNIN", user);
+        localStorage.setItem("token", token);
+        localStorage.setItem("refreshToken", refreshToken);
+        this.setState({ login: true });
+        this.setState({ firstname: user.firstname });
+        this.setState({ lastname: user.lastname });
+        this.setState({ admin: user.admin });
+        this.setState({ profilepicture: user.company.profilepicture });
+        this.setState({ employees: user.company.employees });
       }
-      catch(err) {
-        this.setState({login:  false})
-        localStorage.setItem("token", "");
-        localStorage.setItem("refreshToken", "");
-        console.log("LoginError", err)
-        this.setState({error: filterError(err)})
-      }
+    } catch (err) {
+      this.setState({ login: false });
+      localStorage.setItem("token", "");
+      localStorage.setItem("refreshToken", "");
+      console.log("LoginError", err);
+      this.setState({ error: filterError(err) });
+    }
 
-
-      this.props.history.push("/area/dashboard")
-      //this.props.history.push("/area/advisor")
-
-  }
+    this.props.history.push("/area/dashboard");
+    //this.props.history.push("/area/advisor")
+  };
 
   registerMe = async (email, newsletter) => {
     try {
-      const res = await this.props.signUp({variables: { email, newsletter }})
-        const { ok, token, refreshToken} = res.data.signUp;
-        if (ok) {
-          console.log("SIGNUP", res.data)
-          localStorage.setItem("token", token);
-          localStorage.setItem("refreshToken", refreshToken);
-          console.log("TOKEN", res.data)
-          this.setState({login:  true})
-        }
+      const res = await this.props.signUp({ variables: { email, newsletter } });
+      const { ok, token, refreshToken } = res.data.signUp;
+      if (ok) {
+        console.log("SIGNUP", res.data);
+        localStorage.setItem("token", token);
+        localStorage.setItem("refreshToken", refreshToken);
+        console.log("TOKEN", res.data);
+        this.setState({ login: true });
       }
-      catch(err) {
-        this.setState({login:  false})
-        localStorage.setItem("token", "");
-        localStorage.setItem("refreshToken", "");
-        this.setState({error: filterError(err)})
-        console.log("LoginError", err)
-      }
+    } catch (err) {
+      this.setState({ login: false });
+      localStorage.setItem("token", "");
+      localStorage.setItem("refreshToken", "");
+      this.setState({ error: filterError(err) });
+      console.log("LoginError", err);
+    }
 
-
-      this.props.history.push("/area/advisor")
-
-  }
+    this.props.history.push("/area/advisor");
+  };
 
   render() {
     return (
       <div className="fullSize">
         <Switch>
-          <Route exact path="/" render={
-              (props) => (<Login login={this.logMeIn} register={this.registerMe} error={this.state.error} {...props} />)}/>
-          <Route path="/area" render={(props) => (<Area logMeOut={this.logMeOut} {...props}
-                firstname={this.state.firstname} lastname={this.state.lastname}
-                profilepicture={this.state.profilepicture} admin={this.state.admin} employees={this.state.employees} setName={this.setName}/>) } />
+          <Route
+            exact
+            path="/"
+            render={props => (
+              <Login
+                login={this.logMeIn}
+                register={this.registerMe}
+                error={this.state.error}
+                {...props}
+              />
+            )}
+          />
+          <Route
+            path="/area"
+            render={props => (
+              <Area
+                logMeOut={this.logMeOut}
+                {...props}
+                firstname={this.state.firstname}
+                lastname={this.state.lastname}
+                profilepicture={this.state.profilepicture}
+                admin={this.state.admin}
+                employees={this.state.employees}
+                setName={this.setName}
+              />
+            )}
+          />
           <Route component={Bug} />
         </Switch>
       </div>
@@ -120,5 +136,5 @@ export default compose(
   }),
   graphql(SignUp, {
     name: "signUp"
-  }))
-  (withRouter(App, history))
+  })
+)(withRouter(App, history));
