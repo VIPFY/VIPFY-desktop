24d06b8450c1f35e49cdf1323550306f9eed4d87
wolf <peter@nietz.net>
2018-09-18 23:30:03 +0200

    added Upgrade redirect(s) in iframe #store-iframe

--- src/locationScripts/weebly.ts
+++ src/locationScripts/weebly.ts
@@ -15,57 +15,45 @@ function onLoad() {
 }
 
 function onReady() {
+
   document.querySelectorAll("#upgrade-button")["0"].onmousedown = function(e) {
     e.preventDefault();
     window.location = "vipfy://marketplace/2";
   };
+
   setInterval(modifyAll, 100);
-  /*modifySettings();
-    let url = location.href;
-    document.body.addEventListener('click', ()=>{
-        requestAnimationFrame(()=>{
-          url!==location.href&&modifySettings();
-          url = location.href;
-        });
-    }, true);*/
+
 }
 
 function modifyAll() {
-  //the following two should apply equally to most instances, but there are occasional exceptions
-  /*redirectLinks("#dialog/tier-plan-standalone", todoPath);
-  redirectLinksByQuery("a.tierChangeStandaloneModal", todoPath);
 
-  hideByQuery("#email", true);
-  hideByQuery('a[href="#password"]', true);
-  hideByQuery('a[href="/settings/billing"]', true);
-  hideByQuery('a[href="/settings/change_billing"]', true);
-  hideByQuery('a[href="https://vipfy-test.pipedrive.com/users/add"]', false);
-  hideByQuery('input[name="user[email]"]', true);
-  hideByQuery('a[href="/settings/sso"]', true);*/
+  redirectUpgrade(".Mi4xNS4w36YVA8Z-y-AHr23dzzcTLu_0");
+  redirectUpgrade(".js-upgrade-now");
+  redirectUpgrade(".upgrade-btn");
+
 }
 
-function login() {
-  //clear cookies
-  /*(function() {
-    var cookies = document.cookie.split("; ");
-    for (var c = 0; c < cookies.length; c++) {
-      var d = window.location.hostname.split(".");
-      while (d.length > 0) {
-        var cookieBase =
-          encodeURIComponent(cookies[c].split(";")[0].split("=")[0]) +
-          "=; expires=Thu, 01-Jan-1970 00:00:01 GMT; domain=" +
-          d.join(".") +
-          " ;path=";
-        var p = location.pathname.split("/");
-        document.cookie = cookieBase + "/";
-        while (p.length > 0) {
-          document.cookie = cookieBase + p.join("/");
-          p.pop();
-        }
-        d.shift();
+function redirectUpgrade(whatever: string) {
+  let d = document.querySelectorAll("#store-iframe");
+
+  if (d.length>0) {
+    let ifd = d[0].contentWindow.document;
+
+    if (ifd.body!=null) {
+
+      let elements = ifd.body.querySelectorAll(whatever);
+      for (let i = 0; i < elements.length; i++) {
+        elements[i].onmousedown = function(e) {
+          e.preventDefault();
+          window.location = "vipfy://marketplace/2";
+        };
       }
     }
-  })();*/
+  }
+}
+
+
+function login() {
 
   let ipcRenderer = require("electron").ipcRenderer;
   ipcRenderer.sendToHost("getLoginLink", 2);
