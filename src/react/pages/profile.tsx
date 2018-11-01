f13a973e3067c35e186be79887eb9dd7516d8d8f
Nils Vossebein <nv@vipfy.com>
2018-11-01 13:46:46 +0100

    some frontend stuff

--- src/react/pages/profile.tsx
+++ src/react/pages/profile.tsx
@@ -6,7 +6,7 @@ import AppList from "../components/profile/AppList";
 export default (props: { setApp: Function; id: number; showPopup: Function }) => (
   <div id="profile-page">
     <PersonalData id={props.id} />
-    <CompanyData id={props.id} />
+    {props.isadmin ? <CompanyData id={props.id} /> : ""}
     <AppList setApp={props.setApp} showPopup={props.showPopup} />
   </div>
 );
