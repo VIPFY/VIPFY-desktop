1d71f0900d6ff997b916d50da46310e0ee4a586b
Nils Vossebein <nv@vipfy.com>
2019-01-09 16:55:39 +0100

    SSO stuff

--- src/preload.ts
+++ src/preload.ts
@@ -56,176 +56,176 @@ import { type } from "os";
       require("./locationScripts/dd24.ts")();
       //} else if (hostMatches(".*.?sendgrid.com")) {
       //  require("./locationScripts/sendgrid.ts")();
-    } else if (hostMatches(".*.?freshbooks.com")) {
-      require("./locationScripts/freshbooks.ts")();
-    } else if (hostMatches(".*.?vistaprint.com")) {
-      require("./locationScripts/vistaprint.ts")();
-    } else if (hostMatches(".*.?salesforce.com")) {
-      require("./locationScripts/salesforce.ts")();
+      //} else if (hostMatches(".*.?freshbooks.com")) {
+      //  require("./locationScripts/freshbooks.ts")();
+      //} else if (hostMatches(".*.?vistaprint.com")) {    //NEED TO DO DB WORK
+      //  require("./locationScripts/vistaprint.ts")();
+      //} else if (hostMatches(".*.?salesforce.com")) {
+      //  require("./locationScripts/salesforce.ts")();
     } else if (hostMatches(".*.?webex.com")) {
       require("./locationScripts/webex.ts")();
     } else if (hostMatches(".*.?zendesk.com")) {
       require("./locationScripts/support.ts")();
-    } else if (hostMatches(".*.?meraki.com")) {
-      require("./locationScripts/meraki.ts")();
-    } else if (hostMatches(".*.?docusign.com")) {
-      require("./locationScripts/docusign.ts")();
-    } else if (hostMatches(".*.?amazon.com")) {
-      require("./locationScripts/aws.ts")();
-    } else if (hostMatches(".*.?qbo.intuit.com")) {
-      require("./locationScripts/quickbooks.ts")();
-    } else if (hostMatches(".*.?slack.com")) {
-      require("./locationScripts/slack.ts")();
-    } else if (hostMatches(".*.?realtimeboard.com")) {
-      require("./locationScripts/realtimeboard.ts")();
-    } else if (hostMatches(".*.?37signals.com") || hostMatches(".*.basecamp.com")) {
-      require("./locationScripts/basecamp.ts")();
-    } else if (hostMatches(".*.?humanity.com")) {
-      require("./locationScripts/humanity.ts")();
+      //} else if (hostMatches(".*.?meraki.com")) {
+      //  require("./locationScripts/meraki.ts")();
+      //} else if (hostMatches(".*.?docusign.com")) {
+      //  require("./locationScripts/docusign.ts")();
+      //} else if (hostMatches(".*.?amazon.com")) {     //NEED TO DO DB WORK
+      ///  require("./locationScripts/aws.ts")();
+      //} else if (hostMatches(".*.?qbo.intuit.com")) {   //NEED TO DO DB WORK
+      //  require("./locationScripts/quickbooks.ts")();
+      //} else if (hostMatches(".*.?slack.com")) {
+      //  require("./locationScripts/slack.ts")();
+      //} else if (hostMatches(".*.?realtimeboard.com")) {
+      //  require("./locationScripts/realtimeboard.ts")();
+      //} else if (hostMatches(".*.?37signals.com") || hostMatches(".*.basecamp.com")) {       //NEED TO DO DB WORK
+      //  require("./locationScripts/basecamp.ts")();
+      //} else if (hostMatches(".*.?humanity.com")) {     //NEED TO DO DB WORK
+      //  require("./locationScripts/humanity.ts")();
     } else if (hostMatches(".*.?qualaroo.com")) {
       require("./locationScripts/qualaroo.ts")();
-    } else if (hostMatches(".*.?infusionsoft.com") || hostMatches(".*.?infusionsoft.app")) {
-      require("./locationScripts/infusion.ts")();
-    } else if (hostMatches(".*.?buzzsumo.com")) {
-      require("./locationScripts/buzzsumo.ts")();
-    } else if (hostMatches(".*.?logmeininc.com") || hostMatches(".*.?gotomeeting.com")) {
-      require("./locationScripts/gotomeeting.ts")();
+      //} else if (hostMatches(".*.?infusionsoft.com") || hostMatches(".*.?infusionsoft.app")) {     //NEED TO DO DB WORK
+      //  require("./locationScripts/infusion.ts")();
+      //} else if (hostMatches(".*.?buzzsumo.com")) {        //NEED TO DO DB WORK
+      //  require("./locationScripts/buzzsumo.ts")();
+      //} else if (hostMatches(".*.?logmeininc.com") || hostMatches(".*.?gotomeeting.com")) {      //NEED TO DO DB WORK
+      //  require("./locationScripts/gotomeeting.ts")();
     } else if (hostMatches(".*.?calendly.com")) {
       require("./locationScripts/calendly.ts")();
-    } else if (hostMatches(".*.?teamwork.com")) {
-      require("./locationScripts/teamwork.ts")();
-    } else if (hostMatches(".*.?asana.com")) {
-      require("./locationScripts/asana.ts")();
+      //} else if (hostMatches(".*.?teamwork.com")) {   //NEED TO DO DB WORK
+      //  require("./locationScripts/teamwork.ts")();
+      //} else if (hostMatches(".*.?asana.com")) {
+      //  require("./locationScripts/asana.ts")();
     } else if (hostMatches(".*.?shopify.de")) {
       require("./locationScripts/shopify.ts")();
-    } else if (hostMatches(".*.?expensify.com")) {
-      require("./locationScripts/expensify.ts")();
-    } else if (hostMatches(".*.?typeform.com")) {
-      require("./locationScripts/typeform.ts")();
+      //} else if (hostMatches(".*.?expensify.com")) {      //NEED TO DO DB WORK
+      //  require("./locationScripts/expensify.ts")();
+      //} else if (hostMatches(".*.?typeform.com")) {     //NEED TO DO DB WORK
+      //  require("./locationScripts/typeform.ts")();
     } else if (hostMatches(".*.?surveysparrow.com")) {
       require("./locationScripts/surveysparrow.ts")();
     } else if (hostMatches(".*.?openproject.com")) {
       require("./locationScripts/openproject.ts")();
-    } else if (hostMatches(".*.?getminute.com")) {
-      require("./locationScripts/getminute.ts")();
+      //} else if (hostMatches(".*.?getminute.com")) {      //NEED TO DO DB WORK
+      //  require("./locationScripts/getminute.ts")();
     } else if (hostMatches(".*.?supersaas.de")) {
       require("./locationScripts/supersaas.ts")();
-    } else if (hostMatches(".*.?papershift.com")) {
-      require("./locationScripts/papershift.ts")();
+      //} else if (hostMatches(".*.?papershift.com")) {    //NEED TO DO DB WORK
+      //  require("./locationScripts/papershift.ts")();
     } else if (hostMatches(".*.?sevdesk.de")) {
       require("./locationScripts/sevdesk.ts")();
-    } else if (hostMatches(".*.?freshworks.com")) {
-      require("./locationScripts/freshworks.ts")();
-    } else if (hostMatches(".*.?freedcamp.com")) {
-      require("./locationScripts/freedcamp.ts")();
+      //} else if (hostMatches(".*.?freshworks.com")) {      //NEED TO DO DB WORK
+      //  require("./locationScripts/freshworks.ts")();
+      //} else if (hostMatches(".*.?freedcamp.com")) {    //NEED TO DO DB WORK
+      //  require("./locationScripts/freedcamp.ts")();
     } else if (hostMatches(".*.?zoho.com")) {
       require("./locationScripts/zoho.ts")();
-    } else if (hostMatches(".*.?pipelinedeals.com")) {
-      require("./locationScripts/pipelinedeals.ts")();
-    } else if (hostMatches(".*.?agilecrm.com")) {
-      require("./locationScripts/agilecrm.ts")();
-    } else if (hostMatches(".*.?ladesk.com")) {
-      require("./locationScripts/liveagent.ts")();
+      //} else if (hostMatches(".*.?pipelinedeals.com")) {        //NEED TO DO DB WORK
+      //  require("./locationScripts/pipelinedeals.ts")();
+      //} else if (hostMatches(".*.?agilecrm.com")) {         //NEED TO DO DB WORK
+      //  require("./locationScripts/agilecrm.ts")();
+      //} else if (hostMatches(".*.?ladesk.com")) {    //NEED TO DO DB WORK
+      //  require("./locationScripts/liveagent.ts")();
     } else if (hostMatches(".*.?livechatinc.com")) {
       require("./locationScripts/livechat.ts")();
-    } else if (hostMatches(".*.?groovehq.com")) {
-      require("./locationScripts/groove.ts")();
+      //} else if (hostMatches(".*.?groovehq.com")) {    //NEED TO DO DB WORK
+      //  require("./locationScripts/groove.ts")();
     } else if (hostMatches(".*.?scoop.it")) {
       require("./locationScripts/scoop.ts")();
-    } else if (hostMatches(".*.?instapage.com")) {
-      require("./locationScripts/instapage.ts")();
-    } else if (hostMatches(".*.?landingi.com")) {
-      require("./locationScripts/landingi.ts")();
+      //} else if (hostMatches(".*.?instapage.com")) {    //NEED TO DO DB WORK
+      //  require("./locationScripts/instapage.ts")();
+      //} else if (hostMatches(".*.?landingi.com")) {     //NEED TO DO DB WORK
+      //  require("./locationScripts/landingi.ts")();
     } else if (hostMatches(".*.?vwo.com")) {
       require("./locationScripts/vwo.ts")();
-    } else if (hostMatches(".*.?appcues.com")) {
-      require("./locationScripts/appcues.ts")();
-    } else if (hostMatches(".*.?inlinemanual.com")) {
-      require("./locationScripts/inlinemanual.ts")();
-    } else if (hostMatches(".*.?hotjar.com")) {
-      require("./locationScripts/hotjar.ts")();
+      //} else if (hostMatches(".*.?appcues.com")) {     //NEED TO DO DB WORK
+      //  require("./locationScripts/appcues.ts")();
+      //} else if (hostMatches(".*.?inlinemanual.com")) {    //NEED TO DO DB WORK
+      //  require("./locationScripts/inlinemanual.ts")();
+      //} else if (hostMatches(".*.?hotjar.com")) {    //NEED TO DO DB WORK
+      //  require("./locationScripts/hotjar.ts")();
     } else if (hostMatches(".*.?fullstory.com")) {
       require("./locationScripts/fullstory.ts")();
-    } else if (hostMatches(".*.?mailchimp.com")) {
-      require("./locationScripts/mailchimp.ts")();
+      //} else if (hostMatches(".*.?mailchimp.com")) {    //NEED TO DO DB WORK
+      //  require("./locationScripts/mailchimp.ts")();
     } else if (hostMatches(".*.?shortstackapp.com")) {
       require("./locationScripts/shortstack.ts")();
-    } else if (hostMatches(".*.?amplitude.com")) {
-      require("./locationScripts/amplitude.ts")();
-    } else if (hostMatches(".*.?mixpanel.com")) {
-      require("./locationScripts/mixpanel.ts")();
+      //} else if (hostMatches(".*.?amplitude.com")) {      //NEED TO DO DB WORK
+      //  require("./locationScripts/amplitude.ts")();
+      //} else if (hostMatches(".*.?mixpanel.com")) {    //NEED TO DO DB WORK
+      //  require("./locationScripts/mixpanel.ts")();
     } else if (hostMatches(".*.?statcounter.com")) {
       require("./locationScripts/statcounter.ts")();
     } else if (hostMatches(".*.?woopra.com")) {
       require("./locationScripts/woopra.ts")();
-    } else if (hostMatches(".*.?gosquared.com")) {
-      require("./locationScripts/gosquared.ts")();
+      //} else if (hostMatches(".*.?gosquared.com")) {      //NEED TO DO DB WORK
+      //  require("./locationScripts/gosquared.ts")();
     } else if (hostMatches(".*.?seranking.com")) {
       require("./locationScripts/seranking.ts")();
     } else if (hostMatches(".*.?socialbakers.com")) {
       require("./locationScripts/socialbakers.ts")();
       //} else if (hostMatches(".*.?facebook.com")) {
       //  require("./locationScripts/facebook.ts")();
-    } else if (hostMatches(".*.?instagram.com")) {
-      require("./locationScripts/instagram.ts")();
-    } else if (hostMatches(".*.?twitter.com")) {
-      require("./locationScripts/twitter.ts")();
-    } else if (hostMatches(".*.?linkedin.com")) {
-      require("./locationScripts/linkedin.ts")();
-    } else if (hostMatches(".*.?buffer.com")) {
-      require("./locationScripts/buffer.ts")();
+      //} else if (hostMatches(".*.?instagram.com")) {   //NEED TO DO DB WORK
+      //  require("./locationScripts/instagram.ts")();
+      //} else if (hostMatches(".*.?twitter.com")) {    //NEED TO DO DB WORK
+      //  require("./locationScripts/twitter.ts")();
+      //} else if (hostMatches(".*.?linkedin.com")) {    //NEED TO DO DB WORK
+      //  require("./locationScripts/linkedin.ts")();
+      //} else if (hostMatches(".*.?buffer.com")) {     //NEED TO DO DB WORK
+      //  require("./locationScripts/buffer.ts")();
     } else if (hostMatches(".*.?statusbrew.com")) {
       require("./locationScripts/statusbrew.ts")();
-    } else if (hostMatches(".*.?brand24.com")) {
-      require("./locationScripts/brand24.ts")();
-    } else if (hostMatches(".*.?agorapulse.com")) {
-      require("./locationScripts/agorapulse.ts")();
-    } else if (hostMatches(".*.?eclincher.com")) {
-      require("./locationScripts/eclincher.ts")();
-    } else if (hostMatches(".*.?icontact.com")) {
-      require("./locationScripts/icontact.ts")();
+      //} else if (hostMatches(".*.?brand24.com")) {        //NEED TO DO DB WORK
+      //  require("./locationScripts/brand24.ts")();
+      //} else if (hostMatches(".*.?agorapulse.com")) {      //NEED TO DO DB WORK
+      //  require("./locationScripts/agorapulse.ts")();
+      //} else if (hostMatches(".*.?eclincher.com")) {     //NEED TO DO DB WORK
+      //  require("./locationScripts/eclincher.ts")();
+      //} else if (hostMatches(".*.?icontact.com")) {      //NEED TO DO DB WORK
+      //  require("./locationScripts/icontact.ts")();
     } else if (hostMatches(".*.?sendible.com")) {
       require("./locationScripts/sendible.ts")();
-    } else if (hostMatches(".*.?createsend.com")) {
-      require("./locationScripts/campainmonitor.ts")();
+      //} else if (hostMatches(".*.?createsend.com")) {
+      //  require("./locationScripts/campainmonitor.ts")();    //NEED TO DO DB WORK
     } else if (hostMatches(".*.?robly.com")) {
       require("./locationScripts/robly.ts")();
     } else if (hostMatches(".*.?sendpulse.com")) {
       require("./locationScripts/sendpulse.ts")();
-    } else if (hostMatches(".*.?esignlive.eu")) {
-      require("./locationScripts/esignlive.ts")();
+      //} else if (hostMatches(".*.?esignlive.eu")) {          //NEED TO DO DB WORK
+      //  require("./locationScripts/esignlive.ts")();
     } else if (hostMatches(".*.?signnow.com")) {
       require("./locationScripts/signnow.ts")();
     } else if (hostMatches(".*.?getsigneasy.com")) {
       require("./locationScripts/signeasy.ts")();
-    } else if (hostMatches(".*.?insuresign.com")) {
-      require("./locationScripts/insuresign.ts")();
-    } else if (hostMatches(".*.?eversign.com")) {
-      require("./locationScripts/eversign.ts")();
-    } else if (hostMatches(".*.?pandadoc.com")) {
-      require("./locationScripts/pandadoc.ts")();
-    } else if (hostMatches(".*.?e-sign.co.uk")) {
-      require("./locationScripts/esign.ts")();
-    } else if (hostMatches(".*.?getaccept.com")) {
-      require("./locationScripts/getaccept.ts")();
+      //} else if (hostMatches(".*.?insuresign.com")) {    //NEED TO DO DB WORK
+      //  require("./locationScripts/insuresign.ts")();
+      //} else if (hostMatches(".*.?eversign.com")) {        //NEED TO DO DB WORK
+      //  require("./locationScripts/eversign.ts")();
+      //} else if (hostMatches(".*.?pandadoc.com")) {    //NEED TO DO DB WORK
+      //  require("./locationScripts/pandadoc.ts")();
+      //} else if (hostMatches(".*.?e-sign.co.uk")) {       //NEED TO DO DB WORK
+      //  require("./locationScripts/esign.ts")();
+      //} else if (hostMatches(".*.?getaccept.com")) {       //NEED TO DO DB WORK
+      //  require("./locationScripts/getaccept.ts")();
     } else if (hostMatches(".*.?polleverywhere.com")) {
       require("./locationScripts/polleverywhere.ts")();
     } else if (hostMatches(".*.?sli.do")) {
       require("./locationScripts/slido.ts")();
-    } else if (hostMatches(".*.?glisser.com")) {
-      require("./locationScripts/glisser.ts")();
-    } else if (hostMatches(".*.?eventzilla.net")) {
-      require("./locationScripts/eventzilla.ts")();
-    } else if (hostMatches(".*.?grenadine.co")) {
-      require("./locationScripts/grenadine.ts")();
+      //} else if (hostMatches(".*.?glisser.com")) {       //NEED TO DO DB WORK
+      //  require("./locationScripts/glisser.ts")();
+      //} else if (hostMatches(".*.?eventzilla.net")) {        //NEED TO DO DB WORK
+      //  require("./locationScripts/eventzilla.ts")();
+      //} else if (hostMatches(".*.?grenadine.co")) {       //NEED TO DO DB WORK
+      //  require("./locationScripts/grenadine.ts")();
     } else if (hostMatches(".*.?meetingmatrix.com")) {
       require("./locationScripts/meetingmatrix.ts")();
-    } else if (hostMatches(".*.?allseated.com")) {
-      require("./locationScripts/allseated.ts")();
-    } else if (hostMatches(".*.?activehosted.com")) {
-      require("./locationScripts/activehosted.ts")();
-    } else if (hostMatches(".*.?omnisend.com")) {
-      require("./locationScripts/omnisend.ts")();
+      //} else if (hostMatches(".*.?allseated.com")) {        //NEED TO DO DB WORK
+      //  require("./locationScripts/allseated.ts")();
+      //} else if (hostMatches(".*.?activehosted.com")) {   //NEED TO DO DB WORK
+      //  require("./locationScripts/activehosted.ts")();
+      //} else if (hostMatches(".*.?omnisend.com")) {    //NEED TO DO DB WORK
+      //  require("./locationScripts/omnisend.ts")();
     } else if (hostMatches(".*.?callrail.com")) {
       require("./locationScripts/callrail.ts")();
     } else {
