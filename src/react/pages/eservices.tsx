9ed60010566e1aa89d79a64caaee92751a6d42ff
Nils Vossebein <nv@vipfy.com>
2019-01-31 11:13:22 +0100

    add assigned Services

--- src/react/pages/eservices.tsx
+++ src/react/pages/eservices.tsx
@@ -1,4 +1,10 @@
 import * as React from "react";
+import { Query } from "react-apollo";
+import { fetchUsersOwnLicences } from "../queries/departments";
+import { now } from "moment";
+import gql from "graphql-tag";
+import moment = require("moment");
+import humanizeDuration = require("humanize-duration");
 
 interface Props {}
 
@@ -33,64 +39,107 @@ class EServices extends React.Component<Props, State> {
             <div className="appExplain">
               <div className="heading">Apps:</div>
               <div className="heading">Teamname:</div>
+              <div className="heading">Used since</div>
               <div className="heading">Average usage</div>
+              <div className="heading">Remove Licence</div>
             </div>
-            <div className="appExplain">
-              <div
-                className="box28"
-                style={{
-                  marginRight: "5px",
-                  backgroundImage:
-                    "url('https://storage.googleapis.com/vipfy-imagestore-01/icons/05012019-t3es1-wunderlist-png')"
-                }}
-              />
-              <div>Wunderlist Test Team</div>
-              <div>20 hours/week</div>
-            </div>
-            <div className="appExplain">
-              <div
-                className="box28"
-                style={{
-                  backgroundImage:
-                    "url('https://storage.googleapis.com/vipfy-imagestore-01/icons/pipedrive.png')"
-                }}
-              />
-              <div>Pipedrive Test Team</div>
-              <div>20 hours/week</div>
-            </div>
-            <div className="appExplain">
-              <div
-                className="box28"
-                style={{
-                  backgroundImage:
-                    "url('https://storage.googleapis.com/vipfy-imagestore-01/icons/05012019-s9ysb-wrike-icon-jpg')"
-                }}
-              />
-              <div>Wrike Test Team</div>
-              <div>20 hours/week</div>
-            </div>
-            <div className="appExplain">
-              <div
-                className="box28"
-                style={{
-                  backgroundImage:
-                    "url('https://storage.googleapis.com/vipfy-imagestore-01/icons/05012019-08rvj-smartlook-icon-jpg')"
-                }}
-              />
-              <div>Smartlook Test Team</div>
-              <div>20 hours/week</div>
-            </div>
-            <div className="appExplain">
-              <div
-                className="box28"
-                style={{
-                  backgroundImage:
-                    "url('https://storage.googleapis.com/vipfy-imagestore-01/icons/13102018-ephyv-webex-logo-jpg')"
-                }}
-              />
-              <div>Cisco Webex</div>
-              <div>20 hours/week</div>
-            </div>
+            <Query
+              query={fetchUsersOwnLicences}
+              variables={{ unitid: 22 }}
+              fetchPolicy="network-only">
+              {({ loading, error, data }) => {
+                if (loading) {
+                  return "Loading...";
+                }
+                if (error) {
+                  return `Error! ${error.message}`;
+                }
+                let appArray: JSX.Element[] = [];
+
+                if (data.fetchUsersOwnLicences) {
+                  console.log("D", data.fetchUsersOwnLicences);
+                  data.fetchUsersOwnLicences.forEach((e, k) => {
+                    if (!e.disabled && (e.endtime > now() || e.endtime == null)) {
+                      appArray.push(
+                        <div className="appExplain" key={k}>
+                          <div
+                            className="box28"
+                            style={{
+                              marginRight: "5px",
+                              backgroundImage: `url('https://storage.googleapis.com/vipfy-imagestore-01/icons/${
+                                e.boughtplanid.planid.appid.icon
+                              }')`
+                            }}
+                          />
+                          <div>{e.boughtplanid.alias}</div>
+                          <div>{moment(e.starttime - 0).format("DD.MM.YYYY")}</div>
+                          <Query
+                            query={gql`
+                              query fetchBoughtplanUsagePerUser(
+                                $starttime: Date!
+                                $endtime: Date!
+                                $boughtplanid: ID!
+                              ) {
+                                fetchBoughtplanUsagePerUser(
+                                  starttime: $starttime
+                                  endtime: $endtime
+                                  boughtplanid: $boughtplanid
+                                ) {
+                                  unit {
+                                    id
+                                    firstname
+                                    middlename
+                                    lastname
+                                    title
+                                    profilepicture
+                                  }
+                                  totalminutes
+                                }
+                              }
+                            `}
+                            variables={{
+                              starttime: moment()
+                                .subtract(4, "weeks")
+                                .format("LLL"),
+                              endtime: moment().format("LLL"),
+                              boughtplanid: e.boughtplanid.id
+                            }}>
+                            {({ data, loading, error }) => {
+                              if (loading) {
+                                return <div>Loading</div>;
+                              }
+                              if (error) {
+                                return <div>Error fetching data</div>;
+                              }
+                              if (data) {
+                                console.log(data);
+                                return (
+                                  <div>
+                                    {(data.fetchBoughtplanUsagePerUser.find(e => e.unit.id == 22) &&
+                                      `${humanizeDuration(
+                                        (data.fetchBoughtplanUsagePerUser.find(e => e.unit.id == 22)
+                                          .totalminutes *
+                                          60 *
+                                          1000) /
+                                          28,
+                                        { largest: 1, round: true }
+                                      )} per Day`) ||
+                                      "Not used at all"}
+                                  </div>
+                                );
+                              }
+                            }}
+                          </Query>
+                          <button className="naked-button genericButton">Remove Licence</button>
+                        </div>
+                      );
+                    }
+                  });
+                  return appArray;
+                }
+              }}
+            </Query>
+
             <div className="appExplain">
               <button
                 className="naked-button genericButton box28"
