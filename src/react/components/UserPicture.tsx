34a06be43c1a0a89eb81d441eab91c48d4d8f1d9
Nils Vossebein <nv@vipfy.com>
2019-04-15 19:46:30 +0200

    bugfixes

--- src/react/components/UserPicture.tsx
+++ src/react/components/UserPicture.tsx
@@ -43,10 +43,10 @@ export default function UserPicture(props: {
         let picture = "";
         picture = user.profilepicture
           ? user.profilepicture.indexOf("/") != -1
-            ? `url(https://s3.eu-central-1.amazonaws.com/userimages.vipfy.store/${encodeURI(
+            ? `https://s3.eu-central-1.amazonaws.com/userimages.vipfy.store/${encodeURI(
                 user.profilepicture
-              )})`
-            : `url(${encodeURI(unitPicFolder + user.profilepicture)})`
+              )}`
+            : encodeURI(unitPicFolder + user.profilepicture)
           : defaultPic;
         //user.profilepicture ? unitPicFolder + user.profilepicture : defaultPic;
         const style = {
