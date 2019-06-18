f082c37820c1b5c891b884a4ded8e3884db3e06d
gh05d <pc@vipfy.com>
2019-06-18 13:15:09 +0200

    VIP-279 #time 20m #close exchanged invalid property layoutvertical against valid property sidebar

--- src/react/components/Sidebar.tsx
+++ src/react/components/Sidebar.tsx
@@ -322,8 +322,8 @@ class Sidebar extends React.Component<SidebarProps, State> {
 
     const filteredLicences0 = licences.filter(licence => {
       // Make sure that every License has an index
-      if (licence.layoutvertical === null) {
-        licence.layoutvertical = maxValue + 1;
+      if (licence.sidebar === null) {
+        licence.sidebar = maxValue + 1;
       }
       if (licence.disabled || (licence.endtime && moment().isAfter(licence.endtime))) {
         return false;
