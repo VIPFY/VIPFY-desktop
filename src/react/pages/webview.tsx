cf221c91460fa1f5ee40b00358fa75856c96da3d
Nils Vossebein <nv@vipfy.com>
2019-06-19 19:38:16 +0200

    delete one line

--- src/react/pages/webview.tsx
+++ src/react/pages/webview.tsx
@@ -635,7 +635,6 @@ export class Webview extends React.Component<WebViewProps, WebViewState> {
             //onConsoleMessage={e => console.log("LOGCONSOLE", e.message)}
             onDidNavigateInPage={e => this.onDidNavigateInPage(e.target.src)}
             useragent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"
-            canGoBack={e => console.log("WEBVIEW BACK", e)}
           />
         )}
         {this.state.error ? (
