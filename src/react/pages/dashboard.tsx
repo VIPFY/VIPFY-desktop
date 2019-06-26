9bbbe3667bc1f9093b42c95669dd6bf9007d0b0b
gh05d <pc@vipfy.com>
2019-06-26 19:34:03 +0200

    VIP-326 #time 45m #close implemented Dashboard

--- src/react/pages/dashboard.tsx
+++ src/react/pages/dashboard.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import AppList from "../components/profile/AppList";
 import LoadingDiv from "../components/LoadingDiv";
 import { ErrorComp, filterError, filterAndSort } from "../common/functions";
+import UniversalSearchBox from "../components/universalSearchBox";
+import { Link } from "react-router-dom";
 
 interface Props {
   firstname: string;
@@ -18,28 +20,49 @@ interface Props {
   statisticData: object;
 }
 
-export default (props: Props) => {
-  const setApp = (licence: number) => props.setApp(licence);
+class Dashboard extends React.Component {
+  state = { search: "" };
 
-  if (props.licences.loading) {
-    return <LoadingDiv text="Fetching Licences..." />;
+  render() {
+    const setApp = (licence: number) => this.props.setApp(licence);
+
+    if (this.props.licences.loading) {
+      return <LoadingDiv text="Fetching Licences..." />;
+    }
+
+    if (this.props.licences.error) {
+      return <ErrorComp error={filterError(this.props.licences.error)} />;
+    }
+
+    if (this.props.licences.length < 1) {
+      return (
+        <div className="no-apps">
+          <div>This is your</div>
+          <h1>DASHBOARD</h1>
+          <div>It's a central point of information about your connected services and licenses.</div>
+          <img src={`${__dirname}/../../images/dashboard.png`} alt="Cool pic of a dashboard" />
+          <div>You haven't integrated any services yet.</div>
+          <div>
+            Go to <Link to="/area/integrations">Integrating Accounts</Link> to integrate your
+            services.
+          </div>
+        </div>
+      );
+    }
+
+    const filteredLicences = filterAndSort(this.props.licences.fetchLicences, "dashboard");
+
+    return (
+      <div className="dashboard">
+        <div className="heading">
+          <h1>Dashboard</h1>
+          <UniversalSearchBox getValue={v => this.setState({ search: v })} />
+        </div>
+
+        <AppList search={this.state.search} licences={filteredLicences} setApp={setApp} />
+      </div>
+    );
   }
+}
 
-  if (props.licences.error) {
-    return <ErrorComp error={filterError(props.licences.error)} />;
-  }
-
-  if (props.licences.length < 1) {
-    return <div className="noApp">No Apps for you at the moment :(</div>;
-  }
-
-  const filteredLicences = filterAndSort(props.licences.fetchLicences, "dashboard");
-
-  return (
-    <div className="dashboard-working">
-      <div className="dashboardHeading" />
-
-      <AppList licences={filteredLicences} setApp={setApp} />
-    </div>
-  );
-};
+export default Dashboard;
