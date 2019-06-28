3f8b3a92ed68eb26dc23c483300910bbb5098883
gh05d <pc@vipfy.com>
2019-06-28 15:23:12 +0200

    Fix

--- src/react/pages/usagestatisticsboughtplans.tsx
+++ src/react/pages/usagestatisticsboughtplans.tsx
@@ -17,11 +17,7 @@ interface State {
 }
 
 class UsageStatistics extends React.Component<Props, State> {
-  state = {
-    error: "",
-    showBoughtplans: true,
-    teamname: null
-  };
+  state = { error: "", showBoughtplans: true, teamname: null };
 
   toggleShowBoughtplans = (): void =>
     this.setState(prevState => ({ showBoughtplans: !prevState.showBoughtplans }));
