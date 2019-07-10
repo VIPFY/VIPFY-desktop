5573c6a07deafe14205ded9834ffebe2d238fafa
Jannis Froese <jf@vipfy.com>
2019-07-10 17:57:16 +0200

    better click tracking

--- src/ssoConfigPreload/universalLogin.js
+++ src/ssoConfigPreload/universalLogin.js
@@ -357,3 +357,119 @@ function filterDom(includesAny, excludesAll) {
     return false;
   };
 }
+
+document.addEventListener("mousedown", onClick, true);
+(function() {
+  Element.prototype._addEventListener = Element.prototype.addEventListener;
+  Element.prototype.addEventListener = function(a, b, c) {
+    if (c == undefined) c = false;
+    this._addEventListener(a, b, c);
+    if (!this.eventListenerList) this.eventListenerList = {};
+    if (!this.eventListenerList[a]) this.eventListenerList[a] = [];
+    //this.removeEventListener(a,b,c); // TODO - handle duplicates..
+    this.eventListenerList[a].push({ listener: b, useCapture: c });
+  };
+
+  Element.prototype.getEventListeners = function(a) {
+    if (!this.eventListenerList) this.eventListenerList = {};
+    if (a == undefined) return this.eventListenerList;
+    return this.eventListenerList[a];
+  };
+  Element.prototype.clearEventListeners = function(a) {
+    if (!this.eventListenerList) this.eventListenerList = {};
+    if (a == undefined) {
+      for (var x in this.getEventListeners()) this.clearEventListeners(x);
+      return;
+    }
+    var el = this.getEventListeners(a);
+    if (el == undefined) return;
+    for (var i = el.length - 1; i >= 0; --i) {
+      var ev = el[i];
+      this.removeEventListener(a, ev.listener, ev.useCapture);
+    }
+  };
+
+  Element.prototype._removeEventListener = Element.prototype.removeEventListener;
+  Element.prototype.removeEventListener = function(a, b, c) {
+    if (c == undefined) c = false;
+    this._removeEventListener(a, b, c);
+    if (!this.eventListenerList) this.eventListenerList = {};
+    if (!this.eventListenerList[a]) this.eventListenerList[a] = [];
+
+    // Find the event in the list
+    for (var i = 0; i < this.eventListenerList[a].length; i++) {
+      if (
+        (this.eventListenerList[a][i].listener == b, this.eventListenerList[a][i].useCapture == c)
+      ) {
+        // Hmm..
+        this.eventListenerList[a].splice(i, 1);
+        break;
+      }
+    }
+    if (this.eventListenerList[a].length == 0) delete this.eventListenerList[a];
+  };
+})();
+
+function hasEventHandler(t, e) {
+  return (
+    t["on" + e] ||
+    t.getAttribute("on" + e) ||
+    (t.getEventListeners(e) && t.getEventListeners(e).length > 0)
+  );
+}
+
+function elemIsButton(t) {
+  if (!t) {
+    return false;
+  }
+  if (t.tagName == "BUTTON" || t.tagName == "INPUT" || t.tagName == "A") {
+    return true;
+  }
+
+  if (
+    hasEventHandler(t, "click") ||
+    hasEventHandler(t, "mousedown") ||
+    hasEventHandler(t, "dragstart")
+  ) {
+    return true;
+  }
+  return false;
+}
+
+function onClick(e) {
+  let t = e.target;
+  let isButton = true;
+  while (t && !elemIsButton(t) && t.parentElement != null) {
+    t = t.parentElement;
+  }
+  if (!t || t.parentElement == null) {
+    t = e.target;
+    isButton = false;
+  }
+  let elementX, elementY, elementW, elementH;
+  if (t) {
+    const b = t.getBoundingClientRect();
+    elementX = b.left;
+    elementY = b.top;
+    elementW = b.width;
+    elementH = b.height;
+  }
+  console.log(t);
+  t.style.outline = "solid pink 1px";
+  console.log({
+    eventType: "mc",
+    mouseX: e.clientX,
+    mouseY: e.clientY,
+    windowW: window.innerWidth,
+    windowH: window.innerHeight,
+    elementX,
+    elementY,
+    elementW,
+    elementH,
+    isButton,
+    time: e.timeStamp
+  });
+  if (!isButton) {
+    console.warn("clicked nonbutton", t);
+  }
+}
