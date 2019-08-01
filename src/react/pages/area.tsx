15f70331618353ed98ee154491e4ab9311f1c2a4
gh05d <pc@vipfy.com>
2019-08-01 20:46:53 +0200

    bugfix

--- src/react/pages/area.tsx
+++ src/react/pages/area.tsx
@@ -352,7 +352,7 @@ class Area extends React.Component<AreaProps, AreaState> {
   render() {
     const { sidebarOpen, chatOpen } = this.state;
     const routes = [
-      { path: "", component: Security },
+      { path: "", component: Dashboard },
       { path: "dashboard", component: Dashboard },
       { path: "dashboard/:overlay", component: Dashboard },
       { path: "settings", component: Settings },
