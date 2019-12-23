b489bb5a59978c6e8c269ad7d4231366f7b61a7a
gh05d <pc@vipfy.com>
2019-12-23 18:10:28 +0100

    VIP-852 #time 10m #code #comment improved freshworks logic, hid button

--- src/ssoConfigPreload/freshdesk.js
+++ src/ssoConfigPreload/freshdesk.js
@@ -3,6 +3,10 @@ const sleep = async ms => new Promise(resolve => setTimeout(resolve, ms / speed)
 
 window.addEventListener("load", onLoad);
 
+function hideNodes(nodes) {
+  nodes.forEach(node => (node.style.display = "none"));
+}
+
 function onLoad() {
   if (window.location.pathname != "/support/tickets/new") {
     ipcRenderer.sendToHost("form-submit");
@@ -11,17 +15,15 @@ function onLoad() {
   ipcRenderer.sendToHost("get-email");
   const header = document.querySelector("header");
   const footer = document.querySelector("footer");
-  const copyright = document.querySelector(".copyright");
   const navigation = document.querySelector("nav.page-tabs");
+  const copyright = document.querySelector(".copyright");
+  const cancelButton = document.querySelector("a.btn[href='/support/home']");
 
   const emailWrapper = document.querySelector(".control-group");
   const emailField = document.getElementById("helpdesk_ticket_email");
 
   ipcRenderer.on("email", async (e, email) => {
-    header.style.display = "none";
-    footer.style.display = "none";
-    navigation.style.display = "none";
-    copyright.style.display = "none";
+    hideNodes([header, footer, navigation, copyright, cancelButton]);
 
     emailField.value = email;
     emailWrapper.style.height = 0;
