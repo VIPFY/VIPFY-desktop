a4a53bd98da45291d79afc3fcdbfa0b3f4c99f7d
Jannis Froese <jf@vipfy.store>
2020-01-03 15:29:47 +0100

    VIP-644 #time 1h more debug functionality

--- src/react/components/admin/crytpodebug.tsx
+++ src/react/components/admin/crytpodebug.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import * as crypto from "../../common/crypto";
+import { randomBytes } from "crypto";
+import { getMyEmail } from "../../common/functions";
+import { withApollo } from "react-apollo";
 
 function CryptoDebug(props) {
   const [message, setMessage] = useState("");
@@ -20,6 +23,10 @@ function CryptoDebug(props) {
         <br />
         <label>
           Decrypted: <input size={128} />
+        </label>
+        <br />
+        <label>
+          Public: <input size={128} />
         </label>
         <br />
         <button
@@ -52,9 +59,100 @@ function CryptoDebug(props) {
           }}>
           Encrypt
         </button>
+        <button
+          onClick={async e => {
+            e.preventDefault();
+            try {
+              const form: HTMLInputElement[] = e.target.form;
+              form[0].value = "";
+
+              const r = await crypto.generateNewKeypair();
+              form[1].value = r.privateKey.toString("hex");
+              form[2].value = r.publicKey.toString("hex");
+
+              setMessage(null);
+            } catch (error) {
+              console.error(error, error.stack);
+              setMessage(error.toString());
+            }
+          }}>
+          Generate
+        </button>
+      </form>
+      <h2>LoginKeys</h2>
+      <form>
+        <label>
+          Salt: <input size={128} />
+        </label>
+        <br />
+        <label>
+          Original: <input size={128} />
+        </label>
+        <br />
+        <label>
+          LoginKey: <input size={128} />
+        </label>
+        <br />
+        <label>
+          EncryptionKey: <input size={128} />
+        </label>
+        <br />
+        <button
+          onClick={async e => {
+            e.preventDefault();
+            try {
+              const salt = randomBytes(16).toString("hex");
+              e.target.form[0].value = salt;
+              setMessage(null);
+            } catch (error) {
+              console.error(error, error.stack);
+              setMessage(error.toString());
+            }
+          }}>
+          Random Salt
+        </button>
+        <button
+          onClick={async e => {
+            e.preventDefault();
+            try {
+              const form: HTMLInputElement[] = e.target.form;
+              const pw = form[1].value;
+              const salt = form[0].value;
+              const r = await crypto.hashPasswordWithParams(pw, {
+                salt: salt,
+                ops: 2,
+                mem: 67108864
+              });
+              form[2].value = r.loginkey.toString("hex");
+              form[3].value = r.encryptionkey1.toString("hex");
+              setMessage(null);
+            } catch (error) {
+              console.error(error, error.stack);
+              setMessage(error.toString());
+            }
+          }}>
+          Hash
+        </button>
+        <button
+          onClick={async e => {
+            e.preventDefault();
+            try {
+              const form: HTMLInputElement[] = e.target.form;
+              const pw = form[1].value;
+              const r = await crypto.hashPassword(props.client, await getMyEmail(props.client), pw);
+              form[2].value = r.loginkey.toString("hex");
+              form[3].value = r.encryptionkey1.toString("hex");
+              setMessage(null);
+            } catch (error) {
+              console.error(error, error.stack);
+              setMessage(error.toString());
+            }
+          }}>
+          Hash with my salt
+        </button>
       </form>
     </section>
   );
 }
 
-export default CryptoDebug;
+export default withApollo(CryptoDebug);
