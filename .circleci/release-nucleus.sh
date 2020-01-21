d7bdbeca0f5766dd2125fa1a9301506c93135d46
gh05d <pc@vipfy.com>
2020-01-21 15:43:47 +0100

    Updated Post request

--- .circleci/release-nucleus.sh
+++ .circleci/release-nucleus.sh
@@ -10,10 +10,10 @@ curl -L -b cookies.txt -c cookies.txt -u jf:$PASSWORD http://release.vipfy.store
 
 # Get the previously built version and save the result in a Variable
 VERSION_ID=$(curl -b cookies.txt http://release.vipfy.store:3030/rest/app/1/channel/$CHANNEL_ID/temporary_releases | jq '.[0].id')
-VERSION_NAME=$(curl -b cookies.txt http://release.vipfy.store:3030/rest/app/1/channel/$CHANNEL_ID/temporary_releases | jq -r '.[0].version')
+VERSION_NAME=$(curl -b cookies.txt http://release.vipfy.store:3030/rest/app/1/channel/$CHANNEL_ID/temporary_releases | jq '.[0].version')
 
 # Trigger the Release
 curl -b cookies.txt -X POST http://release.vipfy.store:3030/rest/app/1/channel/$CHANNEL_ID/temporary_releases/$VERSION_ID/release
 echo "The version is called $VERSION_NAME"
 # Set Rollout to 100%
-curl -b cookies.txt --request POST http://release.vipfy.store:3030/rest/app/1/channel/$CHANNEL_ID/rollout --data "version=$VERSION_NAME" --data "rollout=100"
+curl -b cookies.txt -H "Content-Type: application/json"  --request POST http://release.vipfy.store:3030/rest/app/1/channel/$CHANNEL_ID/rollout --data '{"version": $VERSION_NAME, "rollout": 100}'
