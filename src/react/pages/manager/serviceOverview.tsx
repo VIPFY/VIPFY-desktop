2c6a23e11945f005081b6796878523069d6f4b6d
gh05d <pc@vipfy.com>
2020-02-05 17:21:44 +0100

    VIP-1029 #code #comment Frontend changes to refelect changes in backend

--- src/react/pages/manager/serviceOverview.tsx
+++ src/react/pages/manager/serviceOverview.tsx
@@ -145,7 +145,7 @@ class ServiceOverview extends React.Component<Props, State> {
   printServices(services) {
     const serviceArray: JSX.Element[] = [];
     services.forEach(service => {
-      if (service.app.id == 66) {
+      if (service.app.id == "aeb28408-464f-49f7-97f1-6a512ccf46c2") {
         return;
       }
       const teams = [];
