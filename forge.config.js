c63eec8bc87109ea04988f80e464b63e17581739
gh05d <pc@vipfy.com>
2020-02-12 18:49:24 +0100

    Keep on debugging

--- forge.config.js
+++ forge.config.js
@@ -7,9 +7,10 @@ module.exports = {
       platform: "darwin",
       type: "distribution",
       keychain: "VipfyKeychain.keychain",
-      "pre-embed-provisioning-profile": false,
-      // prettier-ignore
-      "provisioning-profile": '~/339ebdc6-a75d-414e-af71-de5fbd6048df.provisionprofile',
+      "identity-validation": false,
+      // "pre-embed-provisioning-profile": false,
+      // // prettier-ignore
+      // "provisioning-profile": '~/339ebdc6-a75d-414e-af71-de5fbd6048df.provisionprofile',
       identity: "Developer ID Application: VIPFY GmbH (RD6VS27844)"
     }
   },
