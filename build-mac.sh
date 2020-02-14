3af15b0ba867f232b245be500fab03d6cced2aea
gh05d <pc@vipfy.com>
2020-02-14 19:54:12 +0100

    VIP-1033 #comment new custom script #time 3d

---
+++ build-mac.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# Get the current branch
+BRANCH="$(git symbolic-ref HEAD 2>/dev/null)"
+BRANCH=${BRANCH##refs/heads/}
+NUCLEUS_PW=$(cat .env | grep NUCLEUS_PW)
+NUCLEUS_PW=${NUCLEUS_PW##NUCLEUS_PW=}
+
+# echo "{
+#   \"channelID\": "\"$CHANNEL_ID\"",
+#     \"server\": \"dev.api.vipfy.store\",
+#     \"serverSSL\": true,
+#     \"serverPort\": 443,
+#     \"development\": true,
+#     \"stripe\": {
+#         \"live\": \"pk_live_OrfeIMTOFjG5o9S5zm9iYH0x\",
+#         \"dev\": \"pk_test_W9VDDvYKZqcmbgaz7iAcUR9j\"
+#     }
+# }" > config3.json
+
+
+# Login to the Apple and execute the script
+ssh -t nilsvossebein@rotten-fruit.fritz.box '
+  export BRANCH='"'$BRANCH'"';
+  export NUCLEUS_PW='"'$NUCLEUS_PW'"'
+  CHANNEL_ID=92c1a89400e8f1153d46aa73ec4ce4d9
+
+  echo "Successfully logged-in to Mac"
+  cd Documents
+  if ! [[ -d vipfy-desktop ]]; then
+    git clone git@bitbucket.org:vipfymarketplace/vipfy-desktop.git
+  fi
+
+  cd vipfy-desktop
+  git stash
+  git checkout master
+  git fetch
+  git checkout $BRANCH
+  git pull
+  npm i
+
+  echo "Editing config.json"
+  cat config.json
+
+  ./build-linux.sh
+
+  cat config.json
+
+  DEBUG=electron-osx-sign* npm run publish-js
+  npm version prerelease -f
+  git add -A
+  git commit -m"Set Version"
+  echo "Successfully built the App. Uploading now..."
+  chmod +x .circleci/release-nucleus.sh
+  ./.circleci/release-nucleus.sh "$NUCLEUS_PW" "$CHANNEL_ID"
+
+  echo "Successfully uploaded the App to Nucleus"
+  exit
+'
