b5d4916e03004aca36592c16de2e7f54310b353b
gh05d <pc@vipfy.com>
2020-02-18 18:54:31 +0100

    More Mac Debugging

--- .circleci/release-nucleus.sh
+++ .circleci/release-nucleus.sh
@@ -5,7 +5,7 @@ touch cookies.txt
 PASSWORD=$1
 CHANNEL_ID=$2
 VERSION=$(cat package.json | jq -r '.version')
-echo "This is the created VERSION $VERSION"
+echo "This is the created Version $VERSION"
 
 # Log in to nucleus
 echo "Logging into Nucleus"
@@ -14,19 +14,13 @@ sleep 3
 
 # Get the previously built version and save the result in a Variable
 VERSION_IDS=$(curl -b cookies.txt http://release.vipfy.store:3030/rest/app/1/channel/$CHANNEL_ID/temporary_releases | jq --arg VERSION "$VERSION" 'map(select(.version==$VERSION))')
-echo "Found this Versions $VERSION_IDS"
+echo "Found these Versions $VERSION_IDS"
+VERSION_ID=$(echo $VERSION_IDS | jq '.[0].id')
 sleep 1
 
-VERSION_ID=$(echo $VERSION_IDS | jq '.[0].id')
-echo "$VERSION_ID"
+# Trigger the Release
+curl -b cookies.txt -X POST http://release.vipfy.store:3030/rest/app/1/channel/$CHANNEL_ID/temporary_releases/$VERSION_ID/release
+sleep 5
 
-# VERSION_NAME=$(curl -b cookies.txt http://release.vipfy.store:3030/rest/app/1/channel/$CHANNEL_ID/temporary_releases | jq '.[0].version')
-# echo "New Version will be called $VERSION_NAME"
-# sleep 2
-
-# # Trigger the Release
-# curl -b cookies.txt -X POST http://release.vipfy.store:3030/rest/app/1/channel/$CHANNEL_ID/temporary_releases/$VERSION_ID/release
-# sleep 5
-
-# # Set Rollout to 100%
-# curl -b cookies.txt -H "Content-Type: application/json" --request POST http://release.vipfy.store:3030/rest/app/1/channel/$CHANNEL_ID/rollout --data "{\"version\": $VERSION_NAME, \"rollout\": 100}" || true
+# Set Rollout to 100%
+curl -b cookies.txt -H "Content-Type: application/json" --request POST http://release.vipfy.store:3030/rest/app/1/channel/$CHANNEL_ID/rollout --data "{\"version\": $VERSION, \"rollout\": 100}" || true
