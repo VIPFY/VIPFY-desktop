bea82c9a9162d5186be2750720b2d6327c0224da
gh05d <pc@vipfy.com>
2020-02-18 19:06:39 +0100

    More Mac Debugging

--- .circleci/release-nucleus.sh
+++ .circleci/release-nucleus.sh
@@ -14,13 +14,15 @@ sleep 3
 
 # Get the previously built version and save the result in a Variable
 VERSION_IDS=$(curl -b cookies.txt http://release.vipfy.store:3030/rest/app/1/channel/$CHANNEL_ID/temporary_releases | jq --arg VERSION "$VERSION" 'map(select(.version==$VERSION))')
-echo "Found these Versions $VERSION_IDS"
+echo "Found these Versions: $VERSION_IDS"
 VERSION_ID=$(echo $VERSION_IDS | jq '.[0].id')
 sleep 1
 
 # Trigger the Release
+echo "Triggering the release of Version $VERSION with id $VERSION_ID"
 curl -b cookies.txt -X POST http://release.vipfy.store:3030/rest/app/1/channel/$CHANNEL_ID/temporary_releases/$VERSION_ID/release
 sleep 5
 
 # Set Rollout to 100%
-curl -b cookies.txt -H "Content-Type: application/json" --request POST http://release.vipfy.store:3030/rest/app/1/channel/$CHANNEL_ID/rollout --data "{\"version\": $VERSION, \"rollout\": 100}" || true
+echo "Trying to set the rollout to 100%"
+curl -b cookies.txt -H "Content-Type: application/json" --request POST http://release.vipfy.store:3030/rest/app/1/channel/$CHANNEL_ID/rollout --data "{\"version\": \"$VERSION\", \"rollout\": 100}" || true
