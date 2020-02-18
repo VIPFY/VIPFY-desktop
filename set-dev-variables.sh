8c0484c99c37cf6d2390a5c9c58595f30bd7aabb
gh05d <pc@vipfy.com>
2020-02-18 17:29:12 +0100

    Come on bro!

--- set-dev-variables.sh
+++ set-dev-variables.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Make sure to have set CHANNEL_ID, SERVER and DEVELOPMENT in your .env file!"
-CHANNEL_ID=${CHANNEL_ID##CHANNEL_ID=}
-BUILD_SERVER=${BUILD_SERVER##BUILD_SERVER=}
-DEVELOPMENT=${DEVELOPMENT##DEVELOPMENT=}
+CHANNEL_ID=$1
+BUILD_SERVER=$2
+DEVELOPMENT=$3
 
 cat config.json | jq '.channelID = "$CHANNEL_ID"' config.json > config-temp.json && mv config-temp.json config.json
 cat config.json | jq '.server = "$BUILD_SERVER"' config.json > config-temp.json && mv config-temp.json config.json
