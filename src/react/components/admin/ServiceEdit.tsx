cd99099333fb55d2df5ef4dff478fe86fc0cee4a
gh05d <pc@vipfy.com>
2020-02-20 18:22:21 +0100

    VIP-1087 #time #code #comment Made admin panel only accessible for VIPFY admins

--- src/react/components/admin/ServiceEdit.tsx
+++ src/react/components/admin/ServiceEdit.tsx
@@ -20,70 +20,56 @@ interface App {
   hidden: boolean;
 }
 
-interface State {
-  apps: App[];
-  seachingFor: string;
-  showApp: null | number;
-}
+const ServiceEdit = (props: Props) => {
+  const [seachingFor, setSearch] = React.useState("");
+  const [showApp, setShow] = React.useState(null);
 
-class ServiceEdit extends React.Component<Props, State> {
-  state = {
-    apps: [],
-    seachingFor: "",
-    showApp: null
-  };
+  return (
+    <section className="admin">
+      <h1>Select a Service to edit</h1>
+      {showApp ? (
+        <Service appid={props.apps.find(app => app.id == showApp)!.id} />
+      ) : (
+        <React.Fragment>
+          <SearchBox searchFunction={(searchValue: string) => setSearch(searchValue)} />
+          <div className="apps">
+            {props.apps
+              .filter(({ name }) => name.toLowerCase().includes(seachingFor.toLowerCase()))
+              .map(({ name, id, icon, disabled, hidden }) => (
+                <div
+                  title={`${disabled ? "Disabled" : ""} ${hidden ? "Hidden" : ""}`}
+                  key={id}
+                  className={`app ${disabled ? "disabled" : ""} ${hidden ? "hidden" : ""}`}
+                  onClick={() => setShow(id)}>
+                  <img height="100px" width="100px" src={`${preAppImageUrl}${icon}`} alt={name} />
+                  <h3>{name}</h3>
+                </div>
+              ))}
+          </div>
+        </React.Fragment>
+      )}
 
-  searchApp = (searchValue: string) => this.setState({ seachingFor: searchValue });
-
-  render() {
-    const { showApp, seachingFor } = this.state;
-
-    return (
-      <section className="admin">
-        <h1>Select a Service to edit</h1>
-        {showApp ? (
-          <Service appid={this.props.apps.find(app => app.id == showApp)!.id} />
-        ) : (
-          <React.Fragment>
-            <SearchBox searchFunction={this.searchApp} />
-            <div className="apps">
-              {this.props.apps
-                .filter(({ name }) => name.toLowerCase().includes(seachingFor.toLowerCase()))
-                .map(({ name, id, icon, disabled, hidden }) => (
-                  <div
-                    title={`${disabled ? "Disabled" : ""} ${hidden ? "Hidden" : ""}`}
-                    key={id}
-                    className={`app ${disabled ? "disabled" : ""} ${hidden ? "hidden" : ""}`}
-                    onClick={() => this.setState({ showApp: id })}>
-                    <img height="100px" width="100px" src={`${preAppImageUrl}${icon}`} alt={name} />
-                    <h3>{name}</h3>
-                  </div>
-                ))}
-            </div>
-          </React.Fragment>
-        )}
-
-        <button
-          type="button"
-          className="button-nav"
-          onClick={() => {
-            if (showApp) {
-              this.setState({ showApp: null, seachingFor: "" });
-            }
-          }}>
-          <i className="fal fa-arrow-alt-from-right" />
-          {showApp ? <span>Go Back</span> : <Link to="/area/admin">Go Back</Link>}
-        </button>
-      </section>
-    );
-  }
-}
+      <button
+        type="button"
+        className="button-nav"
+        onClick={() => {
+          if (showApp) {
+            setShow(null);
+            setSearch("");
+          }
+        }}>
+        <i className="fal fa-arrow-alt-from-right" />
+        {showApp ? <span>Go Back</span> : <Link to="/area/admin">Go Back</Link>}
+      </button>
+    </section>
+  );
+};
 
 export default () => (
   <Query query={FETCH_APPS}>
     {({ data, loading, error }) => {
       if (loading) {
-        return <LoadingDiv text="Fetching Services..." />;
+        return <LoadingDiv />;
       }
 
       if (error || !data) {
