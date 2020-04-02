f65d06d93b70e65f8b49c7d851443039c1d18c74
gh05d <pc@vipfy.com>
2020-04-02 23:26:23 +0200

    Preparing the build of the mac version

--- src/apple-shit/notarize.js
+++ src/apple-shit/notarize.js
@@ -8,9 +8,11 @@ module.exports = () => {
   }
 
   console.log("Notarizing...");
+  console.log("FIRE: process.env.APPLE_APP_PASSWORD", process.env.APPLE_APP_PASSWORD);
+  console.log("FIRE: process.env.APPLE_ID", process.env.APPLE_ID);
 
   return notarize({
-    appBundleId: "vipfy.desktop.app",
+    appBundleId: "vipfy-app",
     appPath: path.join(`${__dirname}/../../out/VIPFY-darwin-x64/VIPFY.app`),
     appleId: process.env.APPLE_ID,
     appleIdPassword: process.env.APPLE_APP_PASSWORD
