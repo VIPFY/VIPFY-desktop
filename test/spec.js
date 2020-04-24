bd7661d200207e0dcb5cf79793f80e5f5ad94ff2
Eva Kiszka <eva.kiszka@vipfy.store>
2020-04-24 15:15:44 +0200

    #code #comment Use Chai and Chai as Promised.

--- test/spec.js
+++ test/spec.js
@@ -1,16 +1,25 @@
-import {Application} from require("spectron");
-const assert = require("assert");
+const Application = require("spectron").Application;
+const chai = require("chai");
+const chaiAsPromised = require("chai-as-promised");
 const electronPath = require("electron");
 const path = require("path");
 
+chai.should();
+chai.use(chaiAsPromised);
+
+const SECOND = 1000;
+const timeout = 10 * SECOND;
+
 describe("Application launch", function () {
-  this.timeout(10000);
+  this.timeout(this.timeout);
 
   beforeEach(function () {
     this.app = new Application({
       path: electronPath,
       args: [path.join(__dirname, "..")]
     });
+
+    chaiAsPromised.transferPromiseness = this.app.transferPromiseness;
 
     return this.app.start();
   });
@@ -21,9 +30,19 @@ describe("Application launch", function () {
     }
   });
 
-  it("shows an initial window", function () {
-    return this.app.client.getWindowCount().then(function (count) {
-      assert.equal(count, 1);
-    });
+  it("opens a window", function () {
+    return this.app.client
+      .waitUntilWindowLoaded()
+      .getWindowCount()
+      .should.eventually.have.at.least(1)
+      .browserWindow.isMinimized()
+      .should.eventually.be.false.browserWindow.isVisible()
+      .should.eventually.be.true.browserWindow.isFocused()
+      .should.eventually.be.true.browserWindow.getBounds()
+      .should.eventually.have.property("width")
+      .and.be.above(0)
+      .browserWindow.getBounds()
+      .should.eventually.have.property("height")
+      .and.be.above(0);
   });
 });
