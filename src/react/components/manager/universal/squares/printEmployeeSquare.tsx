48eb01dc7a98ad0f7df94974a5db4c91066c5197
Eva Kiszka <eva.kiszka@vipfy.store>
2020-07-15 11:41:23 +0200

    Fix issue where the employee square would not really respect the size given as a prop

--- src/react/components/manager/universal/squares/printEmployeeSquare.tsx
+++ src/react/components/manager/universal/squares/printEmployeeSquare.tsx
@@ -14,7 +14,7 @@ interface Props {
 }
 
 export default (props: Props) => {
-  let { employee, overlayFunction } = props;
+  let { className, employee, hideTitle, overlayFunction, size, fake, onClick, styles } = props;
 
   const getShort = employee => {
     let short = "";
@@ -27,43 +27,43 @@ export default (props: Props) => {
     return short;
   };
 
-  if (props.fake) {
+  if (fake) {
     return (
       <div
         key="fake"
         title="Loading"
-        className={props.className || "managerSquare"}
-        style={Object.assign({ ...(props.styles || {}) }, { backgroundColor: "#F2F2F2" })}></div>
-    );
-  } else {
-    if (!employee) {
-      // handle employee == null for renders without data (happens in login)
-      employee = { firstname: "", lastname: "" };
-    }
-    const size = props.size || 32;
-    const name = concatName(employee);
-
-    return (
-      <div
-        title={props.hideTitle ? null : name}
-        className={props.className || "managerSquare"}
-        style={Object.assign(
-          { ...(props.styles || {}) },
-          employee.profilepicture
-            ? {
-                backgroundImage: getBgImageUser(employee.profilepicture, size),
-                backgroundColor: "unset"
-              }
-            : { backgroundColor: employee.color || "#5d76ff" }
-        )}
-        onClick={() => {
-          if (props.onClick) {
-            props.onClick();
-          }
-        }}>
-        {employee.profilepicture ? "" : getShort(employee)}
-        {overlayFunction && overlayFunction(employee)}
-      </div>
+        className={className || "managerSquare"}
+        style={{ backgroundColor: "#F2F2F2", ...styles }}></div>
     );
   }
+
+  if (!employee) {
+    // handle employee == null for renders without data (happens in login)
+    employee = { firstname: "", lastname: "" };
+  }
+
+  const finalSize = size || 32;
+  const name = concatName(employee);
+
+  const backgroundStyle = employee.profilepicture
+    ? {
+        backgroundImage: getBgImageUser(employee.profilepicture, size),
+        backgroundColor: "unset"
+      }
+    : { backgroundColor: employee.color || "#5d76ff" };
+
+  return (
+    <div
+      title={hideTitle ? null : name}
+      className={className || "managerSquare"}
+      style={{ width: finalSize, height: finalSize, ...backgroundStyle, ...styles }}
+      onClick={() => {
+        if (onClick) {
+          onClick();
+        }
+      }}>
+      {employee.profilepicture ? "" : getShort(employee)}
+      {overlayFunction && overlayFunction(employee)}
+    </div>
+  );
 };
