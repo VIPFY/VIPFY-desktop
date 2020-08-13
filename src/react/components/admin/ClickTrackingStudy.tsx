7cef9b40cbf3d61b01d5c0ef0b5be383b72deb4f
gh05d <pc@vipfy.com>
2020-08-13 12:00:17 +0200

    VIP-1322 #comment Added some statistics and more sort options #time 3h

--- src/react/components/admin/ClickTrackingStudy.tsx
+++ src/react/components/admin/ClickTrackingStudy.tsx
@@ -7,6 +7,7 @@ import LoadingDiv from "../LoadingDiv";
 import { ErrorComp } from "../../common/functions";
 import { Query } from "react-apollo";
 import { WorkAround } from "../../interfaces";
+import Duration from "../../common/duration";
 
 interface Participant {
   id: string;
@@ -25,14 +26,16 @@ const FETCH_STUDY_DATA = gql`
 `;
 
 export default () => {
+  const [sortBy, setSort] = React.useState({ column: "registerDate", direction: "down" });
+
   const tableHeads = [
-    "Email",
-    "Registered on",
-    "Amount Files",
-    "Days used",
-    "1. File sent on",
-    "KB / File",
-    "Received Voucher"
+    { label: "Email" },
+    { label: "Registered on", sort: "registerDate" },
+    { label: "Amount Files", sort: "amount" },
+    { label: "Days used", sort: "daysUsed" },
+    { label: "1. File sent" },
+    { label: "KB / File", sort: "size" },
+    { label: "Received Voucher" }
   ];
 
   function selectIcon(value) {
@@ -45,76 +48,150 @@ export default () => {
     }
   }
 
+  const computeTotalSize = user => (user.totalByteSize / user.amountFiles / 1000).toFixed(2);
+
   return (
     <section className="admin" id="click-tracking-study">
       <h1>Click Tracking Study</h1>
 
-      <table>
-        <thead>
-          <tr>
-            {tableHeads.map(head => (
-              <th key={head}>{head}</th>
-            ))}
-          </tr>
-        </thead>
+      <Query<WorkAround, WorkAround> query={FETCH_STUDY_DATA}>
+        {({ data, loading, error }) => {
+          if (loading) {
+            return <LoadingDiv />;
+          }
 
-        <Query<WorkAround, WorkAround> query={FETCH_STUDY_DATA}>
-          {({ data, loading, error }) => {
-            if (loading) {
-              return <LoadingDiv />;
-            }
+          if (error || !data) {
+            return <ErrorComp error={error} />;
+          }
+          const participants: Participant[] = data.adminFetchStudyData;
+          const list = Object.keys(participants);
+          const vipfyUsers = list.filter(id => participants[id].voucher === null).length;
 
-            if (error || !data) {
-              return <ErrorComp error={error} />;
-            }
-            const participants: Participant[] = data.adminFetchStudyData;
+          return (
+            <React.Fragment>
+              <ul className="statistics">
+                <li>
+                  <span>Total number of participants:</span>
+                  {list.length}
+                </li>
 
-            return (
-              <tbody>
-                {Object.keys(participants).map(id => (
-                  <tr
-                    className={classNames(
-                      { "lazy-user": participants[id].amountFiles == 0 },
-                      { "good-user": selectIcon(participants[id].voucher) == "check" },
-                      { "vipfy-user": selectIcon(participants[id].voucher) == "slash" }
-                    )}
-                    key={id}>
-                    <td>{participants[id].email}</td>
-                    <td>
-                      {participants[id].registrationDate == "-"
-                        ? "-"
-                        : moment(participants[id].registrationDate).format("DD.MM.YY")}
-                    </td>
-                    <td>{participants[id].amountFiles}</td>
-                    <td>{participants[id].dates.length}</td>
-                    <td>
-                      {participants[id].dates.length > 0
-                        ? participants[id].dates.reduce((acc, cV) => {
-                            const momentDate = moment(cV, "DD.MM.YY");
+                <li>
+                  <span>Number of VIPFY participants:</span>
+                  {vipfyUsers}
+                </li>
 
-                            return momentDate.isBefore(moment(acc)) ? cV : acc;
-                          })
-                        : "-"}
-                    </td>
-                    <td>
-                      {participants[id].totalByteSize
-                        ? (
-                            participants[id].totalByteSize /
-                            participants[id].amountFiles /
-                            1000
-                          ).toFixed(2)
-                        : "-"}
-                    </td>
-                    <td>
-                      <i className={`fal fa-user-${selectIcon(participants[id].voucher)}`} />
-                    </td>
+                <li title="VIPFY Participants are subtracted from the total number">
+                  <span>
+                    Regular Participants:
+                    <i className="fal fa-info-circle" />
+                  </span>
+                  {list.length - vipfyUsers}
+                </li>
+
+                <li>
+                  <span>Participants who send data:</span>
+                  {list.filter(id => participants[id].dates.length > 0).length}
+                </li>
+
+                <li title="Without VIPFY Participants">
+                  <span>
+                    Participants who finished the study:
+                    <i className="fal fa-info-circle" />
+                  </span>
+                  {list.filter(id => participants[id].voucher).length}
+                </li>
+              </ul>
+
+              <table>
+                <thead>
+                  <tr>
+                    {tableHeads.map(head => (
+                      <th
+                        key={head.label}
+                        onClick={() =>
+                          head.sort &&
+                          setSort(oldState => ({
+                            column: head.sort,
+                            direction: oldState.direction == "down" ? "up" : "down"
+                          }))
+                        }>
+                        {head.label}
+                        {head.sort && <i className="fal fa-sort" />}
+                      </th>
+                    ))}
                   </tr>
-                ))}
-              </tbody>
-            );
-          }}
-        </Query>
-      </table>
+                </thead>
+
+                <tbody>
+                  {list
+                    .sort((a, b) => {
+                      let first, second;
+
+                      if (sortBy.column == "size") {
+                        first = parseFloat(computeTotalSize(participants[a]));
+                        second = parseFloat(computeTotalSize(participants[b]));
+                      } else if (sortBy.column == "amount") {
+                        first = participants[a].amountFiles;
+                        second = participants[b].amountFiles;
+                      } else if (sortBy.column == "registerDate") {
+                        first = moment(participants[a].registrationDate);
+                        second = moment(participants[b].registrationDate);
+                      } else if (sortBy.column == "daysUsed") {
+                        first = participants[a].dates.length;
+                        second = participants[b].dates.length;
+                      }
+
+                      return sortBy.direction == "down" ? first - second : second - first;
+                    })
+                    .map(id => (
+                      <tr
+                        className={classNames(
+                          { "lazy-user": participants[id].amountFiles == 0 },
+                          { "good-user": selectIcon(participants[id].voucher) == "check" },
+                          { "vipfy-user": selectIcon(participants[id].voucher) == "slash" }
+                        )}
+                        key={id}>
+                        <td>{participants[id].email}</td>
+                        <td>
+                          {participants[id].registrationDate == "-"
+                            ? "-"
+                            : moment(participants[id].registrationDate).format("DD.MM.YY")}
+                        </td>
+                        <td>{participants[id].amountFiles}</td>
+                        <td>{participants[id].dates.length}</td>
+                        <td>
+                          {participants[id].dates.length > 0 ? (
+                            <Duration
+                              postfix=" ago"
+                              timestamp={moment(
+                                participants[id].dates.reduce((acc, cV) => {
+                                  const momentDate = moment(cV, "DD.MM.YY");
+
+                                  return momentDate.isBefore(moment(acc)) ? cV : acc;
+                                }),
+                                "DD.MM.YY"
+                              )}
+                            />
+                          ) : (
+                            "-"
+                          )}
+                        </td>
+                        <td>
+                          {participants[id].totalByteSize
+                            ? computeTotalSize(participants[id])
+                            : "-"}
+                        </td>
+                        <td>
+                          <i className={`fal fa-user-${selectIcon(participants[id].voucher)}`} />
+                        </td>
+                      </tr>
+                    ))}
+                </tbody>
+              </table>
+            </React.Fragment>
+          );
+        }}
+      </Query>
 
       <button className="button-nav">
         <i className="fal fa-arrow-alt-from-right" />
