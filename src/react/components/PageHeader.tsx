33e791ef88b3b03ec83b721a0e0e5abaab526c20
Eva Kiszka <eva.kiszka@vipfy.store>
2020-08-18 14:18:20 +0200

    Implementing remaining parts of page header

--- src/react/components/PageHeader.tsx
+++ src/react/components/PageHeader.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import classNames from "classnames";
 import UniversalButton from "./universalButtons/universalButton";
 import BreadCrumbs from "./BreadCrumbs";
+import UniversalSearchBox from "../components/universalSearchBox";
+import Tag from "../common/Tag";
 
 interface PillButtonProps {
   label: string;
@@ -32,28 +34,54 @@ interface WizardConfig {
   steps: string[];
 }
 
+interface Pagination {
+  currentRowsPerPage: number;
+  selectableRowsPerPage: number[];
+  overallRows: number;
+  currentRowsFrom: number;
+  currentRowsTo: number;
+}
+
 interface PageHeaderProps {
   title: string;
   showBreadCrumbs?: boolean;
   wizardConfig?: WizardConfig;
   buttonConfig?: ButtonConfig;
   searchConfig?: SearchConfig;
+  filterConfig?: any;
+  pagination?: Pagination;
   children?: any;
 }
 
 interface PageHeaderState {
   loading: boolean;
+  activeFilters: string[];
 }
 
 class PageHeader extends React.PureComponent<PageHeaderProps, PageHeaderState> {
   constructor(props) {
     super(props);
-    this.state = { loading: false };
+    this.state = { loading: false, activeFilters: [] };
+  }
+
+  clearFilters() {
+    this.setState({ activeFilters: [] });
   }
 
   render() {
-    const { showBreadCrumbs, title, children, buttonConfig, wizardConfig } = this.props;
-    const { loading } = this.state;
+    const { loading, activeFilters } = this.state;
+    const {
+      showBreadCrumbs,
+      title,
+      children,
+      buttonConfig,
+      wizardConfig,
+      searchConfig,
+      filterConfig,
+      pagination
+    } = this.props;
+
+    const showSecondLine = searchConfig || filterConfig || pagination;
 
     return (
       <div className="pageHeader">
@@ -82,6 +110,29 @@ class PageHeader extends React.PureComponent<PageHeaderProps, PageHeaderState> {
             ></UniversalButton>
           )}
         </div>
+
+        {showSecondLine && (
+          <div className="collectionRow">
+            {searchConfig && <UniversalSearchBox placeholder={searchConfig.text} />}
+            {filterConfig && <div>Filter By</div>}
+            {pagination && <div>Rows per page: {pagination.currentRowsPerPage}</div>}
+          </div>
+        )}
+
+        {activeFilters.length > 0 && (
+          <div className="tagsRow">
+            {activeFilters.map(filter => (
+              <Tag key={filter} className="filter">
+                {filter}
+              </Tag>
+            ))}
+            <span className="verticalSeparator" />
+            <span className="clearFiltersBtn" onClick={() => this.clearFilters()}>
+              Clear all
+            </span>
+          </div>
+        )}
+
         <div className="children">{children}</div>
       </div>
     );
