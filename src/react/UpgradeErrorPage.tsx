f5e4c8e0d72b5b2f571e8694a385ff6a275f7e5f
Eva Kiszka <eva.kiszka@vipfy.store>
2020-10-01 15:29:46 +0200

    exchange UniversalButton for Button in UpgradeError

--- src/react/UpgradeErrorPage.tsx
+++ src/react/UpgradeErrorPage.tsx
@@ -1,8 +1,6 @@
 import * as React from "react";
 import { shell } from "electron";
-import { ErrorPage } from "@vipfy-private/vipfy-ui-lib";
-
-import UniversalButton from "./components/universalButtons/universalButton";
+import { Button, ErrorPage } from "@vipfy-private/vipfy-ui-lib";
 
 /**
  * Shows an error page to a user without a valid VIPFY licence.
@@ -15,10 +13,9 @@ export class UpgradeErrorPage extends React.Component<{}, {}> {
           Make sure you have the latest version of your VIPFY App. You can update your version with
           the following link:
         </p>
-        <UniversalButton
+        <Button
           label="www.vipfy.store/update"
-          type="low"
-          customStyles={{ textTransform: "lowercase", fontSize: "24px", margin: "24px 0 32px" }}
+          style={{ margin: "24px 0 32px" }}
           onClick={() => shell.openExternal("https://www.vipfy.store/update")}
         />
       </ErrorPage>
