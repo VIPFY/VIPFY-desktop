6d1d10f79562e6f1f3cf6e1d47dada5d8a25b121
Eva Kiszka <eva.kiszka@vipfy.store>
2020-10-14 14:37:37 +0200

    Upgrade UI lib version. Use UI lib's Card and CardSection in Payment Address.

--- src/react/pages/billing/paymentAddress.tsx
+++ src/react/pages/billing/paymentAddress.tsx
@@ -3,9 +3,8 @@ import { Component } from "react";
 import { Query } from "@apollo/client/react/components";
 import gql from "graphql-tag";
 import { v4 as uuid } from "uuid";
-import { Checkbox } from "@vipfy-private/vipfy-ui-lib";
+import { Card, CardSection, Checkbox } from "@vipfy-private/vipfy-ui-lib";
 
-import CardSection from "../../components/CardSection";
 import UniversalTextInput from "../../components/universalForms/universalTextInput";
 import UniversalDropdownInput from "../../components/universalForms/universalDropdownInput";
 import UniversalButton from "../../components/universalButtons/universalButton";
@@ -364,7 +363,7 @@ class PaymentAddress extends Component<Props, State> {
                   }}
                 />
                 <div className="paymentAddressHolder">
-                  <div className="card">
+                  <Card>
                     <CardSection
                       style={{
                         display: "flex",
@@ -589,8 +588,9 @@ class PaymentAddress extends Component<Props, State> {
                         />
                       </div>
                     </CardSection>
-                  </div>
-                  <div className="card" style={{ minHeight: "100px" }}>
+                  </Card>
+
+                  <Card style={{ minHeight: "100px" }}>
                     <CardSection
                       style={{
                         display: "flex",
@@ -657,10 +657,12 @@ class PaymentAddress extends Component<Props, State> {
                         }
                       />
                     </CardSection>
-                  </div>
+                  </Card>
+
                   {this.state.unsavedChanges && (
                     <PopupBase small={true}>
-                      <div>You have not saved your changes yet! Do you want to save them?</div>
+                      <div>You have not saved your changes yet. Do you want to save them?</div>
+
                       <UniversalButton
                         type="low"
                         label="Discard"
@@ -669,6 +671,7 @@ class PaymentAddress extends Component<Props, State> {
                           this.props.moveTo(this.state.unsavedChanges.pathname.substring(6));
                         }}
                       />
+
                       <UniversalButton
                         type="high"
                         label="Save"
